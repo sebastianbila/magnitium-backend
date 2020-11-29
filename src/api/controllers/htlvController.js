de383f427f6c9820b38225f4c9e1d7829aabc61f
sebii_sw <bilasebastyan@gmail.com>
2020-11-29 12:22:43 +0200

    update routes

--- src/api/controllers/htlvController.js
+++ src/api/controllers/htlvController.js
@@ -1,132 +1,140 @@
-const { HLTV } = require('hltv')
+const { HLTV } = require('hltv');
 
 async function getMatches(req, res, next) {
   try {
-    const { live } = req.query
+    const { live } = req.query;
 
-    let data = await HLTV.getMatches()
-    if (+live === 1) data = data.filter((item) => item.live === true)
+    let data = await HLTV.getMatches();
+    if (+live === 1) data = data.filter((item) => item.live === true);
 
-    res.status(200).json(data)
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getMatch(req, res, next) {
   try {
-    const { id } = req.params
+    const { id } = req.params;
 
-    let data = await HLTV.getMatch({ id })
-    res.status(200).json(data)
+    let data = await HLTV.getMatch({ id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getMatcheStats(req, res, next) {
   try {
-    const data = await HLTV.getMatchesStats({startDate: '2020-11-29', endDate: '2020-11-30'})
-    res.status(200).json(data)
+    const data = await HLTV.getMatchesStats({ startDate: '2020-11-29', endDate: '2020-11-30' });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getMatchStats(req, res, next) {
   try {
-    const id = req.params.id
-    const data = await HLTV.getMatchStats({id: id})
-    res.status(200).json(data)
+    const id = req.params.id;
+    const data = await HLTV.getMatchStats({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getMatchMapStats(req, res, next) {
   try {
-    const id = req.params.id
-    const data = await HLTV.getMatchMapStats({id: id})
-    res.status(200).json(data)
+    const id = req.params.id;
+    const data = await HLTV.getMatchMapStats({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
-
 
 async function getTeam(req, res, next) {
   try {
-    const id = req.params.id
-    const data = await HLTV.getTeam({id: id})
-    res.status(200).json(data)
+    const id = req.params.id;
+    const data = await HLTV.getTeam({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
-
 async function getTeamStats(req, res, next) {
   try {
-    const id = req.params.id
-    const data = await HLTV.getTeamStats({id: id})
-    res.status(200).json(data)
+    const id = req.params.id;
+    const data = await HLTV.getTeamStats({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getPlayer(req, res, next) {
   try {
-    const id = req.params.id
-    const data = await HLTV.getPlayer({id: id})
-    res.status(200).json(data)
+    const id = req.params.id;
+    const data = await HLTV.getPlayer({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getPlayerByName(req, res, next) {
   try {
-    const name = req.params.name
-    const data = await HLTV.getPlayer({name: name})
-    res.status(200).json(data)
+    const name = req.params.name;
+    const data = await HLTV.getPlayer({ name: name });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 async function getPlayerStats(req, res, next) {
   try {
-    const id = req.params.id
-    const data = await HLTV.getPlayerStats({id: id})
-    res.status(200).json(data)
+    const id = req.params.id;
+    const data = await HLTV.getPlayerStats({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
 async function getEvents(req, res, next) {
   try {
-    const data = await HLTV.getPlayerStats({id: id})
-    res.status(200).json(data)
+    const data = await HLTV.getPlayerStats({ id: id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 async function getOngoingEvents(req, res, next) {
   try {
-    const data = await HLTV.getOngoingEvents()
-    res.status(200).json(data)
+    const data = await HLTV.getOngoingEvents();
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
+
+async function getEvents(req, res, next) {
+  try {
+    const data = await HLTV.getEvents();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+}
+
 async function getEvent(req, res, next) {
   try {
-    const {id} = req.params
-    const data = await HLTV.getEvent({id})
-    res.status(200).json(data)
+    const { id } = req.params;
+    const data = await HLTV.getEvent({ id });
+    res.status(200).json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
@@ -144,4 +152,4 @@ module.exports = {
   getEvents,
   getOngoingEvents,
   getEvent,
-}
+};
--- src/api/routers/hltvRouter.js
+++ src/api/routers/hltvRouter.js
@@ -1,17 +1,18 @@
-const router = require('express').Router()
-const { HltvController } = require('../controllers')
+const router = require('express').Router();
+const { HltvController } = require('../controllers');
 
-router.get('/getMatch/:id', HltvController.getMatch)
-router.get('/getMatches/:id', HltvController.getMatches)
-router.get('/getMatcheStats', HltvController.getMatcheStats)
-router.get('/getMatchStats/:id', HltvController.getMatcheStats)
-router.get('/getMatchMapStats/:id', HltvController.getMatchMapStats)
-router.get('/getTeam/:id', HltvController.getTeam)
-router.get('/getTeamStats/:id', HltvController.getTeamStats)
-router.get('/getPlayer/:id', HltvController.getPlayer)
-router.get('/getPlayerByName/:name', HltvController.getPlayerByName)
-router.get('/getPlayerStats/:id', HltvController.getPlayerStats)
-router.get('/getOngoingEvent', HltvController.getOngoingEvents)
-router.get('/getEvents/:id', HltvController.getEvents)
+router.get('/getMatch/:id', HltvController.getMatch);
+router.get('/getMatches/:id', HltvController.getMatches);
+router.get('/getMatcheStats', HltvController.getMatcheStats);
+router.get('/getMatchStats/:id', HltvController.getMatcheStats);
+router.get('/getMatchMapStats/:id', HltvController.getMatchMapStats);
+router.get('/getTeam/:id', HltvController.getTeam);
+router.get('/getTeamStats/:id', HltvController.getTeamStats);
+router.get('/getPlayer/:id', HltvController.getPlayer);
+router.get('/getPlayerByName/:name', HltvController.getPlayerByName);
+router.get('/getPlayerStats/:id', HltvController.getPlayerStats);
+router.get('/getOngoingEvent', HltvController.getOngoingEvents);
+router.get('/getEvents/', HltvController.getEvents);
+router.get('/getEvent/:id', HltvController.getEvent);
 
-module.exports = router
+module.exports = router;
